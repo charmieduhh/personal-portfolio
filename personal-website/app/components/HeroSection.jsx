a73823925a2aa63e7bd89ccd5ca9290829b6083b
Charmie <charmiedubongco@gmail.com>
2024-05-01 14:35:27 -0700

    basic commit with title and lorem ipsum text

---
+++ personal-website/app/components/HeroSection.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const HeroSection = () => {
+    return (
+        <section>
+            <h1 className="flex justify-center text-6xl font-extrabold mb-4">Hello, I'm Charmie!</h1>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ea, ipsa ut, quod nobis facilis quas neque placeat, vero maxime earum animi dicta temporibus! Officiis perferendis quaerat eligendi perspiciatis a sed.</p>
+        </section>
+    )
+}
+
+export default HeroSection;
---
+++ personal-website/app/page.js
@@ -0,0 +1,10 @@
+"use client"
+import HeroSection from './components/HeroSection';
+
+export default function Home() {
+  return (
+   <div className="h-screen bg-white text-black mt-4">
+    <HeroSection />
+   </div>
+  );
+}
