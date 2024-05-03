a7e5a113e75b3e37f270d17025e4ef3654e55448
Charmie <charmiedubongco@gmail.com>
2024-05-03 14:27:57 -0700

    button and title styling, trying to add profile pic

--- personal-website/app/components/HeroSection.jsx
+++ personal-website/app/components/HeroSection.jsx
@@ -1,11 +1,35 @@
 import React from "react";
+import Image from "next/image";
 
 const HeroSection = () => {
     return (
-        <section>
-            <h1 className="flex justify-center text-6xl font-extrabold mb-4">Hello, I'm Charmie!</h1>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ea, ipsa ut, quod nobis facilis quas neque placeat, vero maxime earum animi dicta temporibus! Officiis perferendis quaerat eligendi perspiciatis a sed.</p>
-        </section>
+        <div className="flex flex-col md:flex-row sm:items-center">
+            <div className="md:w-1/2 md:mr-10 sm:text-left">
+                <h1 className="mb-4 text-3xl sm:text-3xl lg:text-5xl lg:leading-normal font-extrabold whitespace-no-wrap text-transparent">
+                    <span className="bg-gradient-to-r from-blue-600 via-pink-500 to-indigo-400 inline-block text-transparent bg-clip-text">
+                        👋 Hi, I'm Charmie! 
+                    </span>
+                </h1>
+                <p className="text-base sm:text-lg lg: text-xl">I'm a full-stack web developer with a passion for creativity and a drive for building. With expertise in JavaScript, I specialize in crafting projects with React on the frontend, complemented by Node.js and Express.js on the backend, while utilizing both SQL (PostgreSQL) and noSQL (MongoDB) for comprehensive database management. I thrive on learning new technologies and solving problems through project building, drawing from my background in healthcare to foster collaboration, attention to detail, and adaptability.
+                Exploring authenticity through my life's journey has shaped me into who I am today. Whether diving into a new programming language/technology, actively pursuing my hobbies🏂🎤🧘‍♀️🏓, or traveling the world, I immerse myself in my interests with unwavering dedication. As a woman in the technology field, let's collaborate and spark innovation together! 👩‍💻✨
+                </p>
+                <div className="flex flex-col md:flex-row mt-4">
+                    <button className="px-6 py-3 rounded-full mr-4 mb-4 bg-rose-200 hover:bg-indigo-400 text-white border border-white w-full sm:w-fit">Hire Me</button>
+                    <button className="px-6 py-3 rounded-full mr-4 mb-4 bg-rose-600 hover:bg-indigo-800 text-white w-full sm:w-fit">Download CV</button>
+                </div>
+            </div>
+            <div className="md:w-1/2 md:ml-10">
+                <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative md:w-3/4 md:ml-20">
+                    <Image
+                        src="/images/personalPhoto.jpeg"
+                        alt="hero image"
+                        className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+                        width={300}
+                        height={300}
+                    /> 
+                </div>
+            </div>
+        </div>
     )
 }
 
--- personal-website/app/page.js
+++ personal-website/app/page.js
@@ -4,7 +4,10 @@ import HeroSection from './components/HeroSection';
 export default function Home() {
   return (
    <div className="h-screen bg-white text-black mt-4">
-    <HeroSection />
+    <div class="container mx-auto px-12 py-4">
+      <HeroSection />
+    </div>
+    
    </div>
   );
 }
