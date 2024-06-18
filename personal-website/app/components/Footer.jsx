0f5d91d9f4ca4f5d38f92be9fa6670460141882f
Charmie <charmiedubongco@gmail.com>
2024-06-18 13:55:23 -0700

    stacks p divs in footer

--- personal-website/app/components/Footer.jsx
+++ personal-website/app/components/Footer.jsx
@@ -6,7 +6,7 @@ import lightPurpHeart from '../images/lightPurpHeart.png';
 const Footer = () => {
   return (
     <footer className="footer border border-t-indigo-950 border-l-transparent border-r-transparent border-b-transparent text-indigo-950">
-      <div className="container pt-14 pb-10 flex justify-between items-center">
+      <div className="container pt-14 pb-10 flex flex-col justify-between items-center">
         <div className="flex items-center">
           {/* <div className="w-10 h-10 md:w-16 md:h-16 relative mr-4">
             <Image
@@ -17,7 +17,7 @@ const Footer = () => {
             />
           </div> */}
           <p className="text-indigo-950 text-left">About: built with Next.js (App Router), React, Javascript, Tailwind CSS, Resend, Cloudflare hosting</p>
-        </div>
+        </div><br/>
         <p className="text-indigo-950">© 2024 Charmie Dubongco. All rights reserved.</p>
       </div>
     </footer>
